f6e7c6ded4e52d21016cc3bfa6fabbfc362f1906
PhilChen <philchen3000tech@gmail.com>
2024-05-18 22:07:36 +0800

    測試加上顯示名稱

--- src/test/java/com/eastrabbit/taiwannationalidentitynumber/NationalIdentityNumberValidatorTest.java
+++ src/test/java/com/eastrabbit/taiwannationalidentitynumber/NationalIdentityNumberValidatorTest.java
@@ -10,12 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NationalIdentityNumberValidatorTest {
 
+
     @Test
+    @DisplayName("[正確的身分證號碼][驗證格式][驗證成功]")
     public void test1() throws InvalidNationalIdentityNumberException {
         NationalIdentityNumberValidator.valid("A123456789");
     }
 
     @Test
+    @DisplayName("[身分證開頭不是大寫英文][驗證格式][驗證失敗]")
     public void test2() {
         InvalidNationalIdentityNumberException exception = assertThrows(InvalidNationalIdentityNumberException.class, () -> {
             NationalIdentityNumberValidator.valid("a123456789");
@@ -24,6 +27,7 @@ class NationalIdentityNumberValidatorTest {
     }
 
     @Test
+    @DisplayName("[身分證開頭不是大寫英文][驗證格式][驗證失敗]")
     public void test3() {
         InvalidNationalIdentityNumberException exception = assertThrows(InvalidNationalIdentityNumberException.class, () -> {
             NationalIdentityNumberValidator.valid("@123456789");
@@ -32,6 +36,7 @@ class NationalIdentityNumberValidatorTest {
     }
 
     @Test
+    @DisplayName("[錯誤的身分證號碼][驗證格式][驗證失敗]")
     public void test4() {
         InvalidNationalIdentityNumberException exception = assertThrows(InvalidNationalIdentityNumberException.class, () -> {
             NationalIdentityNumberValidator.valid("A000000000");
@@ -40,6 +45,7 @@ class NationalIdentityNumberValidatorTest {
     }
 
     @Test
+    @DisplayName("[錯誤的身分證號碼][驗證格式][驗證失敗]")
     public void test5() {
         InvalidNationalIdentityNumberException exception = assertThrows(InvalidNationalIdentityNumberException.class, () -> {
             NationalIdentityNumberValidator.valid("A123456788");
